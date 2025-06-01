747ac6af7eddae09f074f10c9814a57af8f031d1
TheItsNamless <itsnameless@outlook.com>
2025-06-01 14:19:35 +0200

    Aufgabe 4

--- formular/assistant.py
+++ formular/assistant.py
@@ -33,9 +33,34 @@ def create_form(questions: list[str], filename: str = "form.txt") -> None:
             file.write(f"{question} : \n")
 
 
+def read_form(filename: str = "form.txt") -> dict[str, str]:
+    """
+    Liest ein Formular aus einer Datei und gibt die Fragen und Antworten als Dictionary zurück.
+
+    Args:
+        filename (str, optional): Der Name der Datei, aus der das Formular gelesen werden soll. Standardmäßig "form.txt".
+
+    Returns:
+        dict[str, str]: Ein Dictionary, in dem die Fragen als Schlüssel und die Antworten als Werte gespeichert sind.
+    """
+
+    answers: dict[str, str] = dict()
+
+    with open(filename, "r") as file:
+        for line in file.readlines():
+            question, answer = line.strip().split(" : ")[0:2]
+            answers[question] = answer
+
+    return answers
+
+
 def run():
     print("Hello, Formular Assistant!")
 
-    questions = ask_for_form()
-    create_form(questions)
-    print("Formular erstellt.")
+    #questions = ask_for_form()
+    #create_form(questions)
+    #print("Formular erstellt.")
+
+    answers = read_form()
+    print("Formular gelesen:")
+    print(answers)
